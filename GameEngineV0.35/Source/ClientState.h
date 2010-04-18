029b8145d09521a83caf1453b9ebe55f0a03e095
westleyargentum <westleyargentum@af704e40-745a-32bd-e5ce-d8b418a3b9ef>
2010-04-18 07:56:05 +0000

    Also clientstate

---
+++ GameEngineV0.35/Source/ClientState.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "IGameState.h"
+
+namespace Framework
+{
+
+	// The Client will send input to the server and receive messages detailing
+	// the goings on of the game.
+	class ClientState : public IGameState
+	{
+	public:
+		// Create, init,  and destroy methods -----
+		ClientState( GameStateManager *gsm );
+		virtual ~ClientState( void );
+
+		virtual void Initialize( void );
+		virtual void OnCleanup( void );
+		// --------
+
+		// Add / Remove controllers -----
+		virtual void AddController( Controller *controller );
+		virtual void RemoveController( Controller *controller );
+		// --------
+
+		// Receives messages as a system mite, not via message system.
+		virtual void SendMessage( Message *m );
+
+		// Update!
+		virtual void Update( float dt );
+
+		virtual void Restart( void );
+	};
+}
